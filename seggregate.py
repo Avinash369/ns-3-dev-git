e92ae536e49f0d55b56f8870e561492b3c165736
Avinash369 <avinash.kumar369@gmail.com>
2020-02-12 20:33:20 +0530

    Generated more results

--- seggregate.py
+++ seggregate.py
@@ -1,23 +1,27 @@
 import csv
 
-with open('RateHalvingResult') as csv_file:
+with open('ClassicResult') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter='\t')
     line_count = 0
     csv_data = [row for row in csv_reader]
     fields = csv_data[0]
     csv1_data = []
     csv2_data = []
+    csv3_data = []
+    csv4_data = []
     for row in csv_data[1:]:
         
         
         data = [row[0],row[2],row[4],row[6]]
         print(data)
-        if data[1] <= data[0]:
+        if data[1] < data[0]:
             csv1_data.append(data)
+        elif data[1] == data[0]:
+            csv3_data.append(data)
         else:
             csv2_data.append(data)
 
-file1 = "RH_B<=S.csv"
+file1 = "CL_B<S.csv"
   
 
 with open(file1, 'w') as csvfile: 
@@ -27,7 +31,16 @@ with open(file1, 'w') as csvfile:
     csvwriter.writerows(csv1_data)
 
 
-file2 = "RH_B>S.csv"
+file3 = "CL_B=S.csv"
+  
+
+with open(file3, 'w') as csvfile: 
+
+    csvwriter = csv.writer(csvfile) 
+    csvwriter.writerow(fields) 
+    csvwriter.writerows(csv3_data)
+
+file2 = "CL_B>S.csv"
   
 
 with open(file2,'w') as csvfile: 
